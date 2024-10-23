ff9f76b6f2cfa64326e58b6e8d4f2e6e48de3e33
cesarFarieta <cesarfarietaJr@gmail.com>
2024-10-23 14:17:28 +0000

    not done 10/23

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,70 @@
 public class MyLinkedList
 {
+    private ListNode head;
+	private ListNode size ;
 
+	public MyLinkedList()
+	{
+		head = null; 
+		size = 0;
+	} // end Constructor
+
+    public int size(){
+        return size;
+    }
+    public boolean isEmpty(){
+        if (head== null || size==0)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean add(Object newItem){
+
+        
+        
+        ListNode temp=head;
+        int ogSize=temp.size();
+        while(temp.getNext() != null){
+            temp=temp.getNext();
+        }
+        temp.set(newItem);
+
+
+        if(ogSize!=head.size())
+            return true;
+        else
+            return false;
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public String toString(){
+        String result="";
+        int loco=1;
+        ListNode temp = head;
+        while (temp != null)
+        {
+            result += loco +":"+temp.getValue()+"\n";
+            temp = temp.getNext();
+            loco++;
+        }
+        return result;
+        
+    }
 
 }
