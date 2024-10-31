377c7472fb7331cc8929270a9c608f19940ad510
cesarFarieta <cesarfarietaJr@gmail.com>
2024-10-31 05:17:09 +0000

    Ready For Grading 10/31

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,15 @@
+//Cesar Farieta
+//10/31 pd3
+//In this program I Created a Class with public and private methods. I created an instance of a Class.
+//I created 12 different methods, toString(),MyLinkedList(),size(), isEmpty(), add(Object obj), addFirst(Object obj) 
+ //addLast(Object obj), set(int i, Object obj),get(int i),remove(int i),removeFirst() and removeLast().
+
+
+
+
+
 public class MyLinkedList
-{
+{// start class
     private ListNode head;
 	private int size ;
 
@@ -26,23 +36,17 @@ public class MyLinkedList
         // add the special way to an empty list
         if (size == 0)
             head= new ListNode(newItem,null);
-        else{
-            
+        else{//start else
             ListNode temp=head;
             while(temp.getNext() != null){// start while
-                temp=temp.getNext();
-                size++;
+                temp=temp.getNext();       
             }// end while
             temp.setNext(new ListNode(newItem,null));
-        }
-
-        if(ogSize!=size)
-            return true;
-        else
-            return false;
-
-        
-    }// end add
+        }//end esle
+        size++;
+        return true;
+  
+    } // end add
 
     public boolean addLast(Object newItem){// start addLast
         boolean possible = true;
@@ -52,9 +56,8 @@ public class MyLinkedList
         }// end if 
         else
             possible= false;
-        return possible;
-        
-        
+        size++;
+        return possible; 
     }// end addLast
 
     public Object remove(int i){// start remove
@@ -62,8 +65,7 @@ public class MyLinkedList
             throw new IndexOutOfBoundsException();
         else if(i==0)
             return removeFirst(); 
-        else{
-
+        else{// start else
             ListNode temp = head;
             for(int x =0;x<i-1;x++)
                 temp = temp.getNext();
@@ -73,59 +75,53 @@ public class MyLinkedList
             temp2.setNext(null);
             size--;
             return returnValue;
-        }
+        }// end else
     }// end remove
 
-
-    public Object removeLast(){
+    public Object removeLast(){// start removeLast
         if(size==0)
             return null;
         ListNode temp = head;
-        while(temp.getNext().getNext()!=null){
+        while(temp.getNext().getNext()!=null){// start while
             temp = temp.getNext();
-        }
+        }// end while
         Object returnValue= temp.getNext().getValue();
         temp.setNext(null);
+        size--;
         return returnValue;
-    }
+    }//  end  removeLast
 
     public Object removeFirst(){// start remove First
-        ListNode temp = head;
         if (size==0) 
             return null;
+        ListNode temp = head;
         head= head.getNext();
         Object returnVal=temp.getValue(); 
         temp.setNext(null);
-
+        size--;
         return returnVal;
     }// end removeFirst
 
     public boolean addFirst(Object newItem){// start addFirst
-        head= new ListNode(newItem,null);
+        head= new ListNode(newItem,head);
         size++;
         return true;
     }// end addFirst
 
-
     public Object set(int i, Object newValue){// start set
         ListNode temp = head;
-
         if (i < 0 || i >= size) 
             throw new IndexOutOfBoundsException();
         else{// start else
             for(int x =0;x<i;x++)
                 temp= temp.getNext();
-
-        
             Object reValue =temp.getValue(); 
             temp.setValue(newValue);
             return reValue;
-
+        }// end else
     }// end set
 
-
     public Object get(int i){// start get
-
         ListNode temp = head;
         if (i < 0 || i >= size) 
             throw new IndexOutOfBoundsException();
@@ -133,9 +129,7 @@ public class MyLinkedList
             for(int x =0;x<i;x++)
                 temp= temp.getNext();
         }// end else  
-            
         return temp.getValue();
-
     }// end get
 
     public String toString(){//start toString
@@ -149,7 +143,5 @@ public class MyLinkedList
             loco++;
         }// end while
         return result;
-        
     }// end toString
-
 }// end class
